9a385749292ed30b9e3dfa3ffe0cde709baa8fd2
aztecq <azteccx99@gmail.com>
2021-12-18 22:41:37 +0100

    hej

--- main.php
+++ main.php
@@ -5,21 +5,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Witamy</title>
 </head>
 
 <body>
-    <h1>Witamy na stronie świątecznej!</h1>
     <div id="container">
 
-    
-    <div id="content"></div>
+        <div id="logo">
+            <p>Witamy na stronie świątecznej!</p>
+        </div>
+
+        <div id="menu">
+        <a class="link" href="google.com">
+        <div class="option">dupa</div>
+        </a>
+
+        <a class="link" href="google.com">
+        <div class="option">dupa</div>
+        </a>
+
+        <a class="link" href="google.com">
+        <div class="option">dupa</div>
+        </a>
+
+        <a class="link" href="google.com">
+        <div class="option">dupa</div>
+        </a>
+
+        <div style="clear:both;"></div>
+        </div>
+
+        <div style="display:flex;"></div>
+
+        <div id="content"></div>
 
 
 
     </div>
-    
+
 </body>
 
 </html>
