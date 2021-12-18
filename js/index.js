6cc251acff3ca0b6f7d4fe7f292f26f40b7362d1
Balaszka <balagakontakt@gmail.com>
2021-12-18 23:47:35 +0100

    zmiany

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 function checkValidation(){
+    document.querySelector("#login").style.backgroundColor = "white";
+    document.querySelector("#pass").style.backgroundColor = "white";
     var login = document.querySelector("#login").value
     var pass = document.querySelector("#pass").value
     if(login == "" || pass == "" || login == null || pass == null){
         if(login == ""){
             document.querySelector("#login").style.backgroundColor = "red";
-            $("#login").animate({rotate: '100deg'}, 1000)
+            $("#login").animate({height: '25px', width: '179px'}, 500)
+            $("#login").animate({height: '15px', width: '169px'}, 500)
         }
         if(pass == ""){
             document.querySelector("#pass").style.backgroundColor = "red";
+            $("#pass").animate({height: '25px', width: '179px'}, 500)
+            $("#pass").animate({height: '15px', width: '169px'}, 500)
         }
         return false;
     }
