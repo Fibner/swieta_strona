25a1111730c9ff19cd432dcfe94bd3d7eebd3af8
aztecq <azteccx99@gmail.com>
2021-12-19 20:42:11 +0100

    b

--- createAcc.php
+++ createAcc.php
@@ -4,24 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/createAcc.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+    <title>Utwórz konto</title>
 </head>
 <body>
+    <h1>Utwórz konto na naszej stronie!</h1>
+    <hr>
+
+    <div id="acc">
     <form method="post" action="scripts/signin">
         <div>
-        <label>Login:</label>
+        <label>Login</label><br>
         <input type="text" name="login">
         </div>
+
         <div>
-        <label>Hasło:</label>
+        <label>Email</label><br>
+        <input type="text" name="email"> <span style="font-size: 15px">(niewymagane)</span>
+        </div>
+            
+        <div>
+        <label>Hasło</label><br>
         <input type="password" name="password">
         </div>
+
         <div>
-        <label>Email:</label>
-        <input type="text" name="email"> <span style="font-size: 12px">(niewymagane)</span>
+        <label>Potwierdź hasło</label><br>
+        <input type="password" name="password">
         </div>
-        <div>
-            <input type="submit" value="Utwórz konto">
+
+        <button class="button" value="Utwórz konto" onclick="document.location.href = 'createAcc'">Utwórz konto</button>
+
+    </div>
+    
+           
         </div>
     </form>
 </body>
